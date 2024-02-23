b185a143f6461df569c7c0299da7c4bbbf40bb73
Tasssiooo <tassioribeirofontes@gmail.com>
2024-02-22 23:33:41 -0300

    Most UI done, form modal done, todo db schema done

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+const modal = document.getElementById("myModal");
+
+const add = document.getElementById("add");
+
+const cancel = document.getElementById("cancel");
+
+add.onclick = () => {
+  modal.style.display = "flex";
+}
+
+cancel.onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+};
